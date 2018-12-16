a2d2503fa9e17e8968cd527fc29671c1d0e301f8
Curtis Rueden <ctrueden@wisc.edu>
2018-12-15 19:02:15 -0600

    Travis: make leading whitespace consistent

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,23 +7,23 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = true \
 	-a "$TRAVIS_PULL_REQUEST" = false \
 	-a "$TRAVIS_BRANCH" = master ]
 then
-  echo "== Configuring environment ==" &&
+	echo "== Configuring environment ==" &&
 
-  # Configure SSH. The file .travis/ssh-rsa-key.enc must contain
-  # an encrypted private RSA key for communicating with the git remote.
-  mkdir -p "$HOME/.ssh" &&
-  openssl aes-256-cbc \
+	# Configure SSH. The file .travis/ssh-rsa-key.enc must contain
+	# an encrypted private RSA key for communicating with the git remote.
+	mkdir -p "$HOME/.ssh" &&
+	openssl aes-256-cbc \
 		-K "$encrypted_4fc273333c82_key" \
 		-iv "$encrypted_4fc273333c82_iv" \
 		-in '.travis/ssh-rsa-key.enc' \
 		-out "$HOME/.ssh/id_rsa" -d &&
-  chmod 400 "$HOME/.ssh/id_rsa" &&
+	chmod 400 "$HOME/.ssh/id_rsa" &&
 
-  # Configure git settings.
-  git config --global user.email "travis@travis-ci.com" &&
-  git config --global user.name "Travis CI" &&
+	# Configure git settings.
+	git config --global user.email "travis@travis-ci.com" &&
+	git config --global user.name "Travis CI" &&
 
-  echo "== Pushing generated pages ==" &&
+	echo "== Pushing generated pages ==" &&
 
 	# Clone the repository's gh-pages branch.
 	git clone --quiet --branch gh-pages --depth 1 git@github.com:imagej/list-of-update-sites > /dev/null &&
