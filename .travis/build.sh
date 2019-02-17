9b41d8105d3c8f1c16e48e74acfa43498645addd
Curtis Rueden <ctrueden@wisc.edu>
2019-02-17 07:33:40 -0600

    Travis: check YAML syntax for correctness
    As a starting point: ensure the YAML validates,
    and that there are no duplicate IDs.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+echo "== Validating sites.yml =="
+# Validate the YAML.
+python -c "from yaml import load, Loader; load(open('sites.yml'), Loader=Loader)" || exit 3
+# Check for duplicate IDs.
+duplicates=$(grep '^    id: ' sites.yml | sort | uniq -d)
+if [ "$duplicates" ]
+then
+	echo "Duplicate site IDs:"
+	echo "$duplicates"
+	exit 4
+else
+	echo "No duplicate IDs."
+fi
+
 echo "== Generating legacy pages ==" &&
 python generate-legacy-pages.py &&
 
